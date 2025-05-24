2647c78761f12b52905c227a556f796fe59fa7f9
Noel Kujur <lovenoelkujur@gmail.com>
2025-05-24 18:23:45 +0530

    Inital commit

---
+++ frontend/interview-prep-ai/src/utils/data.js
@@ -0,0 +1,29 @@
+
+
+export const APP_FEATURES = [
+    {
+        id : "01",
+        title : "Tailored Just for You",
+        description : "Get interview questions and model answers specifically tailored to your role, experience level, and areas of expertise. Whether you're a beginner or a seasoned professional, our customized approach ensures you're fully prepared for the questions that matter most in your field."
+    },
+    {
+        id : "02",
+        title : "Learn at Your Own Pace",
+        description : "Expand answers only when you're ready. Dive deeper into any concept instantly with AI-powered explanations, examples, and contextual insights — all at your own speed and learning style."
+    },
+    {
+        id : "03",
+        title : "Capture Your Insights",
+        description : "Add personal notes to any question and pin important ones to the top — keeping your learning organized, personalized, and easy to revisit whenever you need."
+    },
+    {
+        id : "04",
+        title : "Understand the 'Why' Behind Answers",
+        description : "Go beyond just memorizing answers — unlock in-depth concept explanations powered by AI, helping you truly understand the reasoning and master each topic with confidence."
+    },
+    {
+        id : "05",
+        title : "Save Organize and Revisit",
+        description : "Easily save your interview sets, organize them neatly in your dashboard, and seamlessly pick up your preparation right where you left off — anytime, anywhere."
+    },
+];
