6dd33f1ba69a9c1ca30be1b5830cfa853fcd13cb
Noel Kujur <lovenoelkujur@gmail.com>
2025-05-28 13:51:16 +0530

    Learn More Component & Add More Questions added

--- frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
+++ frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
@@ -3,13 +3,14 @@ import { LuPlus } from 'react-icons/lu';
 import {CARD_BG} from "../../utils/data";
 import toast from 'react-hot-toast';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
-import { useNavigate } from 'react-router-dom';
+import { data, useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import SummaryCard from '../../components/Cards/SummaryCard';
 import moment from "moment";
 import Modal from '../../components/Modal';
 import CreateSessionForm from './CreateSessionForm';
+import DeleteAlertContent from '../../components/DeleteAlertContent';
 
 const Dashboard = () => {
 
@@ -30,7 +31,23 @@ const Dashboard = () => {
     }
   };
 
-  const deleteSession = async(sessionData) => {};
+  const deleteSession = async(sessionData) => {
+    try {
+
+      await axiosInstance.delete(API_PATHS.SESSION.DELETE(sessionData?._id));
+
+      toast.success("Session Deleted Successfully.");
+      setOpenDeleteAlert({
+        open : false,
+        data : null,
+      });
+
+      fetchAllSessions();
+      
+    } catch (error) {
+      console.error("Error deleting session data: ", error);
+    }
+  };
 
   useEffect(() => {
     fetchAllSessions();
@@ -76,6 +93,21 @@ const Dashboard = () => {
       >
         <div>
           <CreateSessionForm />
+        </div>
+      </Modal>
+
+      <Modal
+        isOpen={openDeleteAlert?.open}
+        onClose={() => {
+          setOpenDeleteAlert({open : false, data : null});
+        }}
+        title="Delete Alert"
+      >
+        <div className='w-[30vw]'>
+          <DeleteAlertContent 
+            content="Are you sure you want to delete this session details ?"
+            onDelete={() => deleteSession(openDeleteAlert.data)}
+          />
         </div>
       </Modal>
     </DashboardLayout>
--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -10,6 +10,9 @@ import RoleInfoHeader from './Components/RoleInfoHeader';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import QuestionCard from '../../components/Cards/QuestionCard';
+import AIResponsePreview from './Components/AIResponsePreview';
+import Drawer from '../../components/Drawer';
+import SkeletonLoader from '../../components/Loader/SkeletonLoader';
 
 const InterviewPrep = () => {
 
@@ -40,7 +43,28 @@ const InterviewPrep = () => {
   };
 
   // Generate Concept Explanation
-  const generateConceptExplanation = async(question) => {};
+  const generateConceptExplanation = async(question) => {
+    try {
+      setErrorMsg("");
+      setExplanation(null);
+
+      setIsLoading(true);
+      setOpenLearnMoreDrawer(true);
+
+      const response = await axiosInstance.post(API_PATHS.AI.GENERATE_EXPLANATION, {question});
+
+      if(response.data){
+        setExplanation(response.data);
+      }
+
+    } catch (error) {
+      setExplanation(null);
+      setErrorMsg("Failed to generate explanation, Try again later");
+      console.error("Error: ", error);
+    } finally {
+      setIsLoading(false)
+    }
+  };
 
   // Pin Question
   const toggleQuestionPinStatus = async(questionId) => {
@@ -60,7 +84,42 @@ const InterviewPrep = () => {
   };
 
   // Add more questions to the session
-  const uploadMoreQuestions = async() => {};
+  const uploadMoreQuestions = async() => {
+    try {
+      setIsUpdateLoaader(true);
+
+      // Call AI API to generate questions
+      const aiResponse = await axiosInstance.post(API_PATHS.AI.GENERATE_QUESTIONS, {
+        role : sessionData?.role,
+        experience : sessionData?.experience,
+        topicsToFocus : sessionData?.topicsToFocus,
+        numberOfQuestions : 10,
+      });
+
+      // Should be array like [{question, answer}, ...]
+      const generatedQuestions = aiResponse.data;
+
+      const response = await axiosInstance.post(API_PATHS.QUESTION.ADD_TO_SESSION, {
+        sessionId,
+        questions : generatedQuestions,
+      });
+
+      if(response.data){
+        toast.success("Added More Q&A.");
+        fetchSessionDetailsById();
+      }
+
+    } catch (error) {
+      if(error.response && error.response.data.message){
+        setErrorMsg(error.response.data.message);
+      }
+      else{
+        setErrorMsg("Something went wrong! please try again.")
+      }
+    } finally {
+      setIsUpdateLoaader(false)
+    }
+  };
 
   useEffect(() => {
     if(sessionId){
@@ -115,6 +174,25 @@ const InterviewPrep = () => {
                         isPinned={data?.isPinned}
                         onTogglePin={() => toggleQuestionPinStatus(data._id)}
                       />
+
+                      {!isLoading &&
+                        sessionData?.questions?.length == index + 1 && (
+                          <div className='flex items-center justify-center mt-5'>
+                            <button
+                              className='flex items-center gap-3 text-sm text-white font-medium bg-black px-5 py-2 mr-2 rounded text-nowrap cursor-pointer'
+                              disabled={isLoading || isUpdateLoader}
+                              onClick={uploadMoreQuestions}
+                            >
+                              {isUpdateLoader ? (
+                                <SpinnerLoader />
+                              ) : (
+                                <LuListCollapse className='text-lg' />
+                              )}{" "}
+                              Load More
+                            </button>
+                          </div>
+                        )
+                      }
                     </>
                   </motion.div>
                 );
@@ -123,7 +201,23 @@ const InterviewPrep = () => {
           </div>
         </div>
 
-        
+        <div>
+          <Drawer
+            isOpen={openLearnMoreDrawer}
+            onClose={() => setOpenLearnMoreDrawer(false)}
+            title={!isLoading && explanation?.title}
+          >
+            {errorMsg && (
+              <p className='flex gap-2 text-sm text-amber-600 font-medium'>
+                <LuCircleAlert className='mt-1' /> {errorMsg}
+              </p>
+            )}
+            {isLoading && <SkeletonLoader />}
+            {!isLoading && explanation && (
+              <AIResponsePreview content={explanation?.explanation} />
+            )}
+          </Drawer>
+        </div>
       </div>
     </DashboardLayout>
   );
